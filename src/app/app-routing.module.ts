6b52fa32c69c5287f91efcace35f2e5cb078ca65
elkhalih <hassan.el-khalil@roche.com>
2022-08-11 11:55:56 +0200

    added some routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"", component:TestComponent, pathMatch:'full'},{path:"abbapage", component:TestComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
