ff74988c0ec8bc73ff74bda4d8f05ad44324f09a
Ynfiniti <sean_schwarz@gmx.de>
2022-08-12 13:13:49 +0200

    routing for abba page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AbbaTableComponent } from './pages/abba-page/abba-table/abba-table.component';
 import { InternshipTableComponent } from './pages/apprentice-page/internship-table/internship-table.component';
 import { ProfileComponent } from './pages/apprentice-page/profile/profile.component';
 import { LandingComponent } from './pages/landing-page/landing/landing.component';
+import { AddProjectPopUpComponent } from './pages/abba-page/add-project-pop-up/add-project-pop-up.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent, pathMatch: 'full' },
   { path: 'apprentice', component: InternshipTableComponent },
   { path: 'apprentice/profile', component: ProfileComponent },
+  { path: 'abba', component: AbbaTableComponent },
+  { path: 'abba/newProject', component: AddProjectPopUpComponent }
 ];
 
 @NgModule({
