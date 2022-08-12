c9a7a283f95694166f94688a591cb0e5fec9e9ba
elkhalih <hassan.el-khalil@roche.com>
2022-08-12 08:18:27 +0200

    added few comp to apprenticepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InternshipTableComponent } from './pages/apprentice-page/internship-table/internship-table.component';
+import { ProfileComponent } from './pages/apprentice-page/profile/profile.component';
 import { TestComponent } from './test/test.component';
 
-const routes: Routes = [{path:"", component:TestComponent, pathMatch:'full'},{path:"abbapage", component:TestComponent}];
+const routes: Routes = [{path:"", component:TestComponent, pathMatch:'full'},{path:"abbapage", component:TestComponent}, {path:"apprenticepage", component:InternshipTableComponent},{path:"apprenticepage/profile", component:ProfileComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OneWebComponentsAngularModule } from '@one/angular';
 import { TestComponent } from './test/test.component';
+import { InternshipTableComponent } from './pages/apprentice-page/internship-table/internship-table.component';
+import { FormsModule } from '@angular/forms';
+import { ProfileComponent } from './pages/apprentice-page/profile/profile.component';
 
 @NgModule({
-  declarations: [AppComponent, TestComponent],
-  imports: [BrowserModule, AppRoutingModule, OneWebComponentsAngularModule],
+  declarations: [AppComponent, TestComponent, InternshipTableComponent, ProfileComponent],
+  imports: [BrowserModule, AppRoutingModule, OneWebComponentsAngularModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
